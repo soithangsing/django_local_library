d959c50bb8e49628ab035e6e5099960c2fec2ad8
Soi Thangsing <getdatasoi@outlook.com>
2024-08-22 01:29:57 +0530

    Update models.py
    Make Genre, Book, BookInstance models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,75 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
+    )
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('genre-detail', args=[str(self.id)])
+    
+    class Meta:
+        constraints = [
+            UniqueConstraint(Lower('name'),
+            name='genre_name_case_insensitive_unique',
+            violation_error_message = "Genre already exists (case insensitive match)"),
+        ]
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author',on_delete=models.RESTRICT, null=True)
+
+    summary = models.TextField(
+        max_length=1000, help_text="Enter a brief description of the book")
+    isbn = models.CharField('ISBN', max_length=13, unique=True,help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn' '">ISBN number</a>')
+
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('book-detail',args=[str(self.id)])
+    
+import uuid
+class BookInstance(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,help_text="Unique ID for this particular book across whole library")
+
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On load'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length = 1,
+        choices = LOAN_STATUS,
+        blank = True,
+        default='m',
+        help_text='Book availability'
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f'{self.id} ({self.book.title})'
+    
+    # https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Models
