1c552acc7b7897ec202fc28bb5cf439801df7f37
Soi Thangsing <getdatasoi@outlook.com>
2024-08-23 12:06:56 +0530

    Add Language Model and register all models on admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Author, Genre, Book, BookInstance, Language
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(Language)
+
+# Logging in and using the site
+# https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Admin_site
--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.urls import reverse
-# Create your models here.
-
 from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
+# Create your models here.
 
 class Genre(models.Model):
     name = models.CharField(
@@ -23,6 +22,27 @@ class Genre(models.Model):
             UniqueConstraint(Lower('name'),
             name='genre_name_case_insensitive_unique',
             violation_error_message = "Genre already exists (case insensitive match)"),
+        ]
+
+class Language(models.Model):
+    name = models.CharField(max_length=200,
+    unique=True,
+    help_text="Enter book's natural language "                        
+    )
+
+    def get_absolute_url(self):
+        return reverse('language-detail', args=[str(self.id)])
+    
+    def __str__(self):
+        return self.name
+    
+    class meta:
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                name='language_name_case_insensitive_unique',
+                violation_error_message = "Language already exists (case-insensitive match)"
+            )
         ]
 
 class Book(models.Model):
@@ -72,4 +92,17 @@ class BookInstance(models.Model):
     def __str__(self):
         return f'{self.id} ({self.book.title})'
     
-    # https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Models
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+    
+    def __str__(self):
+        return f'{self.last_name},{self.first_name}'
